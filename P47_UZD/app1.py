86762c2f2d021c93fd06488ff1bfa2faef80a109
Eldar-Gi <eldargimb@gmail..com>
2025-01-29 11:03:29 +0200

    Prideta CORS

--- P47_UZD/app1.py
+++ P47_UZD/app1.py
@@ -2,6 +2,7 @@ from models import db, Automobiliai
 from flask import Flask, render_template, request, redirect, url_for, jsonify
 from serialaizers import CarsSchema
 from flask_cors import CORS
+
 app = Flask(__name__)
 CORS(app)
 # Database configuration
@@ -13,24 +14,24 @@ with app.app_context():
     db.create_all()
 
 
-# @app.route("/uzd")
-# def home():
-#     all_autos = Automobiliai.query.all()
-#     cars_json = [{
-#         "id": car.id,
-#         "gamintojas": car.gamintojas,
-#         "modelis": car.modelis,
-#         "spalva": car.spalva,
-#         "pagaminimo_metai": car.pagaminimo_metai,
-#         "kaina": car.kaina
-#     } for car in all_autos]
-#     return jsonify(cars_json)
-
 @app.route("/uzd")
 def home():
     all_autos = Automobiliai.query.all()
-    cars_json = [CarsSchema.model_validate(car, from_attributes=True).model_dump() for car in all_autos]
+    cars_json = [{
+        "id": car.id,
+        "gamintojas": car.gamintojas,
+        "modelis": car.modelis,
+        "spalva": car.spalva,
+        "pagaminimo_metai": car.pagaminimo_metai,
+        "kaina": car.kaina
+    } for car in all_autos]
     return jsonify(cars_json)
+
+# @app.route("/uzd")
+# def home():
+#     all_autos = Automobiliai.query.all()
+#     cars_json = [CarsSchema.model_validate(car, from_attributes=True).model_dump() for car in all_autos]
+#     return jsonify(cars_json)
 
 
 if __name__ == "__main__":
